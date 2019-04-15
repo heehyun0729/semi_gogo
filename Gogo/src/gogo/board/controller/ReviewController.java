9f29d858bc99966b8f9f45d8b846da3c698a914b
JHTA <JHTA@JHTA-PC>
2019-04-15 11:25:23 +0900

    aa

--- Gogo/src/gogo/board/controller/ReviewController.java
+++ Gogo/src/gogo/board/controller/ReviewController.java
@@ -17,6 +17,7 @@ public class ReviewController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String spageNum = req.getParameter("pageNum");
+		
 		String keyword = req.getParameter("keyword");
 		
 		int pageNum = 1; 
