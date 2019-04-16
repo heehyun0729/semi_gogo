084e9a95fa2c14ae4b1a12692ad6e4fcf5a5994c
JHTA <JHTA@JHTA-PC>
2019-04-16 17:00:58 +0900

    delete.jsp css 완료 controller수정 완료

--- Gogo/WebContent/freedom/freedomDelete.jsp
+++ Gogo/WebContent/freedom/freedomDelete.jsp
@@ -5,9 +5,11 @@
 	<form method="post" action="${cp }/freedom/freedomDelete.do">
 		<input type = "hidden" name = "menu_num" value = "${param.menu_num }">
 		<input type = "hidden" name = "freedom_num" value = "${param.freedom_num }">
-		<p>글을 정말로 삭제할까요?</p>
-		<input type = "submit" value = "삭제" >
-		<input type = "button" value = "취소" onclick="javascript:history.go(-1);">
+		<p style="margin-left: 50%">글을 정말로 삭제할까요?</p>
+		<div style="margin-left: 50%">
+			<input class="btn btn-primary" type = "submit" value = "삭제" >
+			<input class="btn btn-primary" type = "button" value = "취소" onclick="javascript:history.go(-1);">
+		</div>
 	</form>
 </div>
 <!--  -->
--- Gogo/src/gogo/freedomController/freedomDeleteController.java
+++ Gogo/src/gogo/freedomController/freedomDeleteController.java
@@ -15,8 +15,10 @@ public class freedomDeleteController extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int freedom_num = Integer.parseInt(req.getParameter("freedom_num"));
-		System.out.println("freedom_num1:" + freedom_num);
+		String menu_num = req.getParameter("menu_num");
+		String freedom_num = req.getParameter("freedom_num");
+		//int freedom_num = Integer.parseInt(req.getParameter("freedom_num"));
+		//System.out.println("freedom_num1:" + freedom_num);
 //		FreedomDao dao=FreedomDao.getInstance();
 //		System.out.println("dao:" + dao);
 ////		int n = dao.delete(freedom_num);
@@ -24,8 +26,12 @@ public class freedomDeleteController extends HttpServlet{
 //			// 오류 처리
 //			System.out.println("freedom DB 삭제 실패");
 //		}else {
-		resp.sendRedirect(req.getContextPath() + "/freedom/freedomDelete.jsp?freedom_num=" + freedom_num);
+//		resp.sendRedirect(req.getContextPath() + "/freedom/freedomDelete.jsp?freedom_num=" + freedom_num);
 //		}
+		req.setAttribute("menu_num", menu_num);
+		req.setAttribute("freedom_num", freedom_num);
+		req.setAttribute("spage", "/freedom/freedomDelete.jsp");
+		req.getRequestDispatcher("/home.jsp").forward(req, resp);
 	}
 
 	
